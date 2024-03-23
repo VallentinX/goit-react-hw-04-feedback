9a1752b540ea9a6c2e2bdb9620f09cf3a2645620
VallentinX <valentin.cristian_98@icloud.com>
2024-03-24 00:57:51 +0200

    2nd Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -42,16 +42,19 @@ const App = () => {
           onLeaveFeedback={onLeaveFeedback}
         />
       </Section>
-
-      <Section title="Statistics">
-        <Statistics
-          good={feedback.good}
-          neutral={feedback.neutral}
-          bad={feedback.bad}
-          total={totalFeedback}
-          positivePercentage={positivePercentage}
-        />
-      </Section>
+      {totalFeedback > 0 ? (
+        <Section title="Statistics">
+          <Statistics
+            good={feedback.good}
+            neutral={feedback.neutral}
+            bad={feedback.bad}
+            total={totalFeedback}
+            positivePercentage={positivePercentage}
+          />
+        </Section>
+      ) : (
+        <Notification message="There is no feedback" />
+      )}
     </div>
   );
 };
