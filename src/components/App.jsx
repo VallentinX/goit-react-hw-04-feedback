64a442289176ad8a175c82d346faa11cd4dab8cc
VallentinX <valentin.cristian_98@icloud.com>
2024-03-24 00:52:11 +0200

    1st Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 
-import Notification from './Notification/Notification';
 import Section from './Section/Section';
 import FeedbackOptions from './FeedbackOption/FeedbackOption';
 import Statistics from './Statistics/Statistics';
@@ -43,19 +42,16 @@ const App = () => {
           onLeaveFeedback={onLeaveFeedback}
         />
       </Section>
-      {totalFeedback > 0 ? (
-        <Section title="Statistics">
-          <Statistics
-            good={feedback.good}
-            neutral={feedback.neutral}
-            bad={feedback.bad}
-            total={totalFeedback}
-            positivePercentage={positivePercentage}
-          />
-        </Section>
-      ) : (
-        <Notification message="There is no feedback" />
-      )}
+
+      <Section title="Statistics">
+        <Statistics
+          good={feedback.good}
+          neutral={feedback.neutral}
+          bad={feedback.bad}
+          total={totalFeedback}
+          positivePercentage={positivePercentage}
+        />
+      </Section>
     </div>
   );
 };
